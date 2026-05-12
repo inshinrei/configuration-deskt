0543c3d78aed05237d359a37ac8441f6f0e3eb97
tokkiumai <kiwidancebad@gmail.com>
2026-05-12 21:04:20 +0300

    update time.sh

--- config/sketchybar_bottombar/plugins/time.sh
+++ config/sketchybar_bottombar/plugins/time.sh
@@ -15,22 +15,22 @@ case $HOUR in
         BCOLOR=$COLOR_CYAN_BRIGHT
         COLOR=$COLOR_BACKGROUND
         ;;
-    [1][0-1])
+    [1][0-6])
         ICON=$ICON_CLOCK
         BCOLOR=$COLOR_BLUE_BRIGHT
         COLOR=$COLOR_BACKGROUND
         ;;
-    [1][2-6])
+    [1][7-8])
         ICON=$ICON_CLOCK
         BCOLOR=$COLOR_ORANGE_BRIGHT
         COLOR=$COLOR_BACKGROUND
         ;;
-    [1][7-8])
+    [1][9])
         ICON=$ICON_CLOCK
         BCOLOR=$COLOR_RED_BRIGHT
         COLOR=$COLOR_BACKGROUND
         ;;
-    [1][9-2][0])
+    [1-2][0])
         ICON=$ICON_CLOCK
         BCOLOR=$COLOR_BLACK
         COLOR=$COLOR_DEFAULT
